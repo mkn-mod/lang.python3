4a149a0b84d54e9ac4c3980b8247f49058522f8d
dekken <philip.deegan@gmail.com>
2019-11-15 14:34:15 +0100

    momo

--- test.cpp
+++ test.cpp
@@ -1,9 +1,33 @@
 
+#include "kul/signal.hpp"
+#include "kul/yaml.hpp"
+#include <maiken.hpp>
 
-#include "Python.h"
-#include "numpy/npy_math.h"
+const std::string yArgs = "with: numpy";
 
 int main(int argc, char* argv[]) {
-  PyObject descr;
-  return 0;
+    kul::Signal sig;
+    try {
+        YAML::Node node = kul::yaml::String(yArgs).root();
+        char* argv2[2] = {argv[0], (char*)"-O"};
+        char* argv3[3] = {argv[0], (char*)"-Op", (char*)"test"};
+        auto app = (maiken::Application::CREATE(2, argv2))[0];
+        auto loader(maiken::ModuleLoader::LOAD(*app));
+        auto appTest = (maiken::Application::CREATE(3, argv3))[0];
+        loader->module()->init(*appTest, node);
+        loader->module()->compile(*appTest, node);
+        loader->module()->link(*appTest, node);
+        loader->module()->pack(*appTest, node);
+        loader->unload();
+    } catch (const kul::Exception& e) {
+        KLOG(ERR) << e.what();
+        return 2;
+    } catch (const std::exception& e) {
+        KERR << e.what();
+        return 3;
+    } catch (...) {
+        KERR << "UNKNOWN EXCEPTION TYPE CAUGHT";
+        return 5;
+    }
+    return 0;
 }
